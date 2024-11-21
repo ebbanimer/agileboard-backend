3594f662a55f61f1a3899ac187829b9d7a31838c
ebbanimer <ebba.nimer@gmail.com>
2024-11-21 21:22:44 +0100

    Initial commit

---
+++ src/test/java/com/example/agileboard/AgileboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agileboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
